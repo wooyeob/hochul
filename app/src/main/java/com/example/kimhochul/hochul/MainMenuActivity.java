bb369137b1bea8c210fe36eb92f7a8c9ec80c799
Kimhomal <hochul1005@naver.com>
2017-02-01 16:08:25 +0900

    project start

---
+++ app/src/main/java/com/example/kimhochul/hochul/MainMenuActivity.java
@@ -0,0 +1,16 @@
+package com.example.kimhochul.hochul;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Kimhochul on 2017-01-23.
+ */
+
+public class MainMenuActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+    }
+}
